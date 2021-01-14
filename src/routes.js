26a349e0999ecea089991f38df91e9cd0b0dfc70
Akshay <53929892+badcoderxd@users.noreply.github.com>
2021-01-14 21:14:47 +0530

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './components/home';
+import Artist from './components/artist';
+
+
+const Routes = () => {
+        return(
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/artist/:id" exact component={Artist}/> 
+            </Switch>  
+        )
+}
+
+export default Routes;
